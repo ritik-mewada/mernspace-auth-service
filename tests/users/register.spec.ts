674e9dabff19cd0bf07ae6ab955df7017c898895
ritik-mewada <ritikmewada@gmail.com>
2024-09-19 19:15:02 -0400

    add test for persist user

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,12 +1,28 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
+import { truncateTables } from "../utils";
 
 describe("POST /auth/register", () => {
     describe("Given all fields", () => {
+        let connection: DataSource;
+
+        beforeAll(async () => {
+            connection = await AppDataSource.initialize();
+        });
+        beforeEach(async () => {
+            await truncateTables(connection);
+        });
+        afterAll(async () => {
+            await connection.destroy();
+        });
+
         it("should return the 200 status code", async () => {
             const userData = {
                 firstName: "Ritik",
-                LastName: "Mewada",
+                lastName: "Mewada",
                 email: "ritik@mern.space",
                 password: "secret",
             };
@@ -21,7 +37,7 @@ describe("POST /auth/register", () => {
         it("should return valid json response", async () => {
             const userData = {
                 firstName: "Ritik",
-                LastName: "Mewada",
+                lastName: "Mewada",
                 email: "ritik@mern.space",
                 password: "secret",
             };
@@ -38,12 +54,20 @@ describe("POST /auth/register", () => {
         it("should persit the user in the database", async () => {
             const userData = {
                 firstName: "Ritik",
-                LastName: "Mewada",
+                lastName: "Mewada",
                 email: "ritik@mern.space",
                 password: "secret",
             };
 
             await request(app).post("/auth/register").send(userData);
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(1);
+            expect(users[0].firstName).toBe(userData.firstName);
+            expect(users[0].lastName).toBe(userData.lastName);
+            expect(users[0].email).toBe(userData.email);
         });
     });
 

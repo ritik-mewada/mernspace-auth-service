b7236d14d127114b0950ee17e4e201b4d4c74383
ritik-mewada <ritikmewada@gmail.com>
2024-10-02 17:24:48 -0400

    add email senitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -171,4 +171,23 @@ describe("POST /auth/register", () => {
             expect(users).toHaveLength(0);
         });
     });
+
+    describe("Fields are not in proper format", () => {
+        it("should trim the email field", async () => {
+            const userData = {
+                firstName: "Ritik",
+                lastName: "Mewada",
+                email: "    ritik@mern.space      ",
+                password: "secret",
+            };
+
+            await request(app).post("/auth/register").send(userData);
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            const user = users[0];
+
+            expect(user.email).toBe("ritik@mern.space");
+        });
+    });
 });

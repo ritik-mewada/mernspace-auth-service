23a1af5a88b0a1b5d672c67fb8baac14f505efeb
ritik-mewada <ritikmewada@gmail.com>
2024-10-09 16:05:41 -0400

    change status code of login endpoint and add test for login

--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -1,5 +1,11 @@
+import request from "supertest";
+import bcrypt from "bcrypt";
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
+import { Roles } from "../../src/constants";
+import app from "../../src/app";
+import { isJwt } from "../utils";
 
 describe("POST /auth/login", () => {
     let connection: DataSource;
@@ -18,6 +24,73 @@ describe("POST /auth/login", () => {
     });
 
     describe("Given all fields.", () => {
-        it.todo("should login the user");
+        it("should return the access token and refresh token inside a cookie", async () => {
+            const userData = {
+                firstName: "Ritik",
+                lastName: "Mewada",
+                email: "ritik@mern.space",
+                password: "password",
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            });
+
+            // Act
+            const response = await request(app)
+                .post("/auth/login")
+                .send({ email: userData.email, password: userData.password });
+
+            interface Headers {
+                ["set-cookie"]?: string[];
+            }
+
+            let accessToken = null;
+            let refreshToken = null;
+            const cookies = (response.headers as Headers)["set-cookie"] || [];
+
+            cookies.forEach((cookie) => {
+                if (cookie.startsWith("accessToken=")) {
+                    accessToken = cookie.split(";")[0].split("=")[1];
+                }
+
+                if (cookie.startsWith("refreshToken=")) {
+                    refreshToken = cookie.split(";")[0].split("=")[1];
+                }
+            });
+            expect(accessToken).not.toBeNull();
+            expect(refreshToken).not.toBeNull();
+
+            expect(isJwt(accessToken)).toBeTruthy();
+            expect(isJwt(refreshToken)).toBeTruthy();
+        });
+        it("should return the 400 if email or password is wrong", async () => {
+            const userData = {
+                firstName: "Ritik",
+                lastName: "Mewada",
+                email: "ritik@mern.space",
+                password: "password",
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            });
+
+            const response = await request(app)
+                .post("/auth/login")
+                .send({ email: userData.email, password: "wrongPassword" });
+
+            expect(response.statusCode).toBe(400);
+        });
     });
 });

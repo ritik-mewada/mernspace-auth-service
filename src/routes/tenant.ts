21bbd6e5e268689e68411c5fa03ac97de4619b8b
ritik-mewada <ritikmewada@gmail.com>
2025-01-11 20:56:46 -0500

    add create tenant endpoint

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,7 +1,17 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { TenantController } from "../controllers/TenantController";
+import { TenantService } from "../services/TenantService";
+import { Tenant } from "../entity/Tenant";
+import { AppDataSource } from "../config/data-source";
+import logger from "../config/logger";
 
 const router = express.Router();
+const tenantRepository = AppDataSource.getRepository(Tenant);
+const tenanatService = new TenantService(tenantRepository);
+const tenantController = new TenantController(tenanatService, logger);
 
-router.post("/", (req: Request, res: Response) => res.status(201).json({}));
+router.post("/", (req: Request, res: Response, next: NextFunction) =>
+    tenantController.create(req, res, next),
+);
 
 export default router;
--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
 import app from "../../src/app";
+import { Tenant } from "../../src/entity/Tenant";
 
 describe("POST /tenants", () => {
     let connection: DataSource;
@@ -31,6 +32,22 @@ describe("POST /tenants", () => {
                 .send(tenantData);
 
             expect(response.statusCode).toBe(201);
+        });
+
+        it("should create a tenant in the database", async () => {
+            const tenantData = {
+                name: "Tenant Name",
+                address: "Tenant Address",
+            };
+
+            await request(app).post("/tenants").send(tenantData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(1);
+            expect(tenants[0].name).toBe(tenantData.name);
+            expect(tenants[0].address).toBe(tenantData.address);
         });
     });
 });

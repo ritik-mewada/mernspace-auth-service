43356bc101aab1f5aa6ebaf01403769e28deb72b
ritik-mewada <ritikmewada@gmail.com>
2025-09-01 17:54:21 -0400

    update user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,4 @@
-import express, {
-    NextFunction,
-    Request,
-    Response,
-    RequestHandler,
-} from "express";
+import express, { NextFunction, RequestHandler, Response } from "express";
 import authenticate from "../middlewares/authenticate";
 import { canAccess } from "../middlewares/canAccess";
 import { Roles } from "../constants";
@@ -13,9 +8,10 @@ import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 import logger from "../config/logger";
 import createUserValidator from "../validators/create-user-validator";
-import listUsersValidator from "../validators/list-users-validator";
+import { CreateUserRequest, UpdateUserRequest } from "../types";
 import updateUserValidator from "../validators/update-user-validator";
-import { UpdateUserRequest } from "../types";
+import listUsersValidator from "../validators/list-users-validator";
+import { Request } from "express-jwt";
 
 const router = express.Router();
 
@@ -26,10 +22,19 @@ const userController = new UserController(userService, logger);
 router.post(
     "/",
     authenticate as RequestHandler,
-    createUserValidator,
     canAccess([Roles.ADMIN]),
-    (req: Request, res: Response, next: NextFunction) =>
+    createUserValidator,
+    (req: CreateUserRequest, res: Response, next: NextFunction) =>
         userController.create(req, res, next) as unknown as RequestHandler,
+);
+
+router.patch(
+    "/:id",
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    updateUserValidator,
+    (req: UpdateUserRequest, res: Response, next: NextFunction) =>
+        userController.update(req, res, next) as unknown as RequestHandler,
 );
 
 router.get(
@@ -45,25 +50,85 @@ router.get(
     "/:id",
     authenticate as RequestHandler,
     canAccess([Roles.ADMIN]),
-    (req: Request, res: Response, next: NextFunction) =>
+    (req, res, next) =>
         userController.getOne(req, res, next) as unknown as RequestHandler,
-);
-
-router.patch(
-    "/:id",
-    authenticate as RequestHandler,
-    canAccess([Roles.ADMIN]),
-    updateUserValidator,
-    (req: UpdateUserRequest, res: Response, next: NextFunction) =>
-        userController.update(req, res, next) as unknown as RequestHandler,
 );
 
 router.delete(
     "/:id",
     authenticate as RequestHandler,
     canAccess([Roles.ADMIN]),
-    (req: Request, res: Response, next: NextFunction) =>
+    (req, res, next) =>
         userController.destroy(req, res, next) as unknown as RequestHandler,
 );
 
 export default router;
+// import express, {
+//     NextFunction,
+//     Request,
+//     Response,
+//     RequestHandler,
+// } from "express";
+// import authenticate from "../middlewares/authenticate";
+// import { canAccess } from "../middlewares/canAccess";
+// import { Roles } from "../constants";
+// import { UserController } from "../controllers/UserController";
+// import { UserService } from "../services/UserService";
+// import { AppDataSource } from "../config/data-source";
+// import { User } from "../entity/User";
+// import logger from "../config/logger";
+// import createUserValidator from "../validators/create-user-validator";
+// import listUsersValidator from "../validators/list-users-validator";
+// import updateUserValidator from "../validators/update-user-validator";
+// import { UpdateUserRequest } from "../types";
+
+// const router = express.Router();
+
+// const userRepository = AppDataSource.getRepository(User);
+// const userService = new UserService(userRepository);
+// const userController = new UserController(userService, logger);
+
+// router.post(
+//     "/",
+//     authenticate as RequestHandler,
+//     createUserValidator,
+//     canAccess([Roles.ADMIN]),
+//     (req: Request, res: Response, next: NextFunction) =>
+//         userController.create(req, res, next) as unknown as RequestHandler,
+// );
+
+// router.get(
+//     "/",
+//     authenticate as RequestHandler,
+//     canAccess([Roles.ADMIN]),
+//     listUsersValidator,
+//     (req: Request, res: Response, next: NextFunction) =>
+//         userController.getAll(req, res, next) as unknown as RequestHandler,
+// );
+
+// router.get(
+//     "/:id",
+//     authenticate as RequestHandler,
+//     canAccess([Roles.ADMIN]),
+//     (req: Request, res: Response, next: NextFunction) =>
+//         userController.getOne(req, res, next) as unknown as RequestHandler,
+// );
+
+// router.patch(
+//     "/:id",
+//     authenticate as RequestHandler,
+//     canAccess([Roles.ADMIN]),
+//     updateUserValidator,
+//     (req: UpdateUserRequest, res: Response, next: NextFunction) =>
+//         userController.update(req, res, next) as unknown as RequestHandler,
+// );
+
+// router.delete(
+//     "/:id",
+//     authenticate as RequestHandler,
+//     canAccess([Roles.ADMIN]),
+//     (req: Request, res: Response, next: NextFunction) =>
+//         userController.destroy(req, res, next) as unknown as RequestHandler,
+// );
+
+// export default router;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,8 @@
 import { Brackets, Repository } from "typeorm";
+import bcrypt from "bcryptjs";
 import { User } from "../entity/User";
 import { LimitedUserData, UserData, UserQueryParams } from "../types";
 import createHttpError from "http-errors";
-import bcrypt from "bcryptjs";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
@@ -19,11 +19,10 @@ export class UserService {
             where: { email: email },
         });
         if (user) {
-            const err = createHttpError(400, "Email already exists");
+            const err = createHttpError(400, "Email is already exists!");
             throw err;
         }
-
-        // hash password
+        // Hash the password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         try {
@@ -38,7 +37,7 @@ export class UserService {
         } catch {
             const error = createHttpError(
                 500,
-                "Failed to store the data in the database!",
+                "Failed to store the data in the database",
             );
             throw error;
         }
@@ -64,42 +63,14 @@ export class UserService {
     }
 
     async findById(id: number) {
-        return this.userRepository.findOne({
-            where: { id },
-            relations: { tenant: true },
+        return await this.userRepository.findOne({
+            where: {
+                id,
+            },
+            relations: {
+                tenant: true,
+            },
         });
-    }
-
-    async getAll(validatedQuery: UserQueryParams) {
-        const queryBuilder = this.userRepository.createQueryBuilder("user");
-
-        if (validatedQuery.q) {
-            const searchTerm = `%${validatedQuery.q}%`;
-            queryBuilder.where(
-                new Brackets((qb) => {
-                    qb.where(
-                        "CONCAT(user.firstName, ' ', user.lastName) ILike :q",
-                        {
-                            q: searchTerm,
-                        },
-                    ).orWhere("user.email ILike :q", { q: searchTerm });
-                }),
-            );
-        }
-
-        if (validatedQuery.role) {
-            queryBuilder.andWhere("user.role = :role", {
-                role: validatedQuery.role,
-            });
-        }
-        const result = await queryBuilder
-            .leftJoinAndSelect("user.tenant", "tenant")
-            .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
-            .take(validatedQuery.perPage)
-            .orderBy("user.id", "DESC")
-            .getManyAndCount();
-
-        return result;
     }
 
     async update(
@@ -123,7 +94,164 @@ export class UserService {
         }
     }
 
+    async getAll(validatedQuery: UserQueryParams) {
+        const queryBuilder = this.userRepository.createQueryBuilder("user");
+
+        if (validatedQuery.q) {
+            const searchTerm = `%${validatedQuery.q}%`;
+            queryBuilder.where(
+                new Brackets((qb) => {
+                    qb.where(
+                        "CONCAT(user.firstName, ' ', user.lastName) ILike :q",
+                        { q: searchTerm },
+                    ).orWhere("user.email ILike :q", { q: searchTerm });
+                }),
+            );
+        }
+
+        if (validatedQuery.role) {
+            queryBuilder.andWhere("user.role = :role", {
+                role: validatedQuery.role,
+            });
+        }
+
+        const result = await queryBuilder
+            .leftJoinAndSelect("user.tenant", "tenant")
+            .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
+            .take(validatedQuery.perPage)
+            .orderBy("user.id", "DESC")
+            .getManyAndCount();
+        return result;
+    }
+
     async deleteById(userId: number) {
         return await this.userRepository.delete(userId);
     }
 }
+// import { Brackets, Repository } from "typeorm";
+// import { User } from "../entity/User";
+// import { LimitedUserData, UserData, UserQueryParams } from "../types";
+// import createHttpError from "http-errors";
+// import bcrypt from "bcryptjs";
+
+// export class UserService {
+//     constructor(private userRepository: Repository<User>) {}
+
+//     async create({
+//         firstName,
+//         lastName,
+//         email,
+//         password,
+//         role,
+//         tenantId,
+//     }: UserData) {
+//         const user = await this.userRepository.findOne({
+//             where: { email: email },
+//         });
+//         if (user) {
+//             const err = createHttpError(400, "Email already exists");
+//             throw err;
+//         }
+
+//         // hash password
+//         const saltRounds = 10;
+//         const hashedPassword = await bcrypt.hash(password, saltRounds);
+//         try {
+//             return await this.userRepository.save({
+//                 firstName,
+//                 lastName,
+//                 email,
+//                 password: hashedPassword,
+//                 role,
+//                 tenant: tenantId ? { id: tenantId } : null,
+//             });
+//         } catch {
+//             const error = createHttpError(
+//                 500,
+//                 "Failed to store the data in the database!",
+//             );
+//             throw error;
+//         }
+//     }
+
+//     async findByEmailWithPassword(email: string) {
+//         return await this.userRepository.findOne({
+//             where: {
+//                 email,
+//             },
+//             select: [
+//                 "id",
+//                 "firstName",
+//                 "lastName",
+//                 "email",
+//                 "role",
+//                 "password",
+//             ],
+//             relations: {
+//                 tenant: true,
+//             },
+//         });
+//     }
+
+//     async findById(id: number) {
+//         return this.userRepository.findOne({
+//             where: { id },
+//             relations: { tenant: true },
+//         });
+//     }
+
+//     async getAll(validatedQuery: UserQueryParams) {
+//         const queryBuilder = this.userRepository.createQueryBuilder("user");
+
+//         if (validatedQuery.q) {
+//             const searchTerm = `%${validatedQuery.q}%`;
+//             queryBuilder.where(
+//                 new Brackets((qb) => {
+//                     qb.where(
+//                         "CONCAT(user.firstName, ' ', user.lastName) ILike :q",
+//                         { q: searchTerm },
+//                     ).orWhere("user.email ILike :q", { q: searchTerm });
+//                 }),
+//             );
+//         }
+
+//         if (validatedQuery.role) {
+//             queryBuilder.andWhere("user.role = :role", {
+//                 role: validatedQuery.role,
+//             });
+//         }
+
+//         const result = await queryBuilder
+//             .leftJoinAndSelect("user.tenant", "tenant")
+//             .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
+//             .take(validatedQuery.perPage)
+//             .orderBy("user.id", "DESC")
+//             .getManyAndCount();
+//         return result;
+//     }
+
+//     async update(
+//         userId: number,
+//         { firstName, lastName, role, email, tenantId }: LimitedUserData,
+//     ) {
+//         try {
+//             return await this.userRepository.update(userId, {
+//                 firstName,
+//                 lastName,
+//                 role,
+//                 email,
+//                 tenant: tenantId ? { id: tenantId } : null,
+//             });
+//         } catch (err) {
+//             const error = createHttpError(
+//                 500,
+//                 "Failed to update the user in the database",
+//             );
+//             throw error;
+//         }
+//     }
+
+//     async deleteById(userId: number) {
+//         return await this.userRepository.delete(userId);
+//     }
+// }
